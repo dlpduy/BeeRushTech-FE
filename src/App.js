2dee7d5ab29adb3dfe1b89db909a962894c45042
hdanhng <danh.nguyenhoang@hcmut.edu.vn>
2024-11-02 21:26:54 +0700

    update: add routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import React, { useEffect } from 'react';
+import CartPage from './Cart/index';
+import PrivacyPolicy from './FAQ/Privacy/PrivacyPolicy';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    // Add Google Fonts link to <head>
+    const link = document.createElement('link');
+    link.href = 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+  }, []); // Run only once when the app initializes
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <div style={{ fontFamily: 'Lato, sans-serif' }}>
+      <Router>
+        <Routes>
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        </Routes>
+      </Router>
+    </div>
   );
-}
+};
 
 export default App;
