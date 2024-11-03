df42122248c4369b58926c3fca09ba98b832f8eb
tinhale <tinhalecshcmut@gmail.com>
2024-11-04 01:00:02 +0700

    change:App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-
+import React from 'react';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cart from './Cart';
 import PrivacyPolicy from './FAQ/Privacy/PrivacyPolicy'
